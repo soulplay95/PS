7e26637ce49866e43c701d7973e35df7a1493e5f
soulplay95 <soulplay95@gmail.com>
2021-10-17 05:25:36 +0900

    feat: Solve SWEA_5644 in Java

---
+++ SWEA/SWEA_5644.java
@@ -0,0 +1,111 @@
+/**
+ * @문제 무선충전
+ * @날짜 211017
+ * @분류 
+ * @조건
+ * #
+ * @풀이
+ * #
+ * @comment
+ * #
+ */
+
+
+import java.util.*;
+import java.io.*;
+
+public class SWEA_5644 {
+
+	static int M, bcCnt;
+	static int[] pathA, pathB, playerA, playerB;
+	static int[][] bc;
+
+	static int[] dx = {0, 0, 1, 0, -1}; // 그대로, 상, 우, 하, 좌
+	static int[] dy = {0, -1, 0, 1, 0};
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int TC = Integer.parseInt(br.readLine());
+		playerA = new int[2]; // 사용자 A의 위치
+		playerB = new int[2]; // 사용자 B의 위치
+
+		for (int tc = 1; tc <= TC; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			M = Integer.parseInt(st.nextToken());
+			bcCnt = Integer.parseInt(st.nextToken());
+
+			// 초기 위치
+			playerA[0] = playerA[1] = 1;
+			playerB[0] = playerB[1] = 10;
+
+			pathA = new int[M + 1]; // 사용자 A의 궤적
+			pathB = new int[M + 1]; // 사용자 B의 궤적
+			bc = new int[bcCnt][4]; // 4 : x, y, 사정거리, 충전량
+
+			StringTokenizer stA = new StringTokenizer(br.readLine(), " ");
+			StringTokenizer stB = new StringTokenizer(br.readLine(), " ");
+
+			for (int i = 1; i <= M; i++) { // i = 0일때 값은 0으로 남아있다.(0 : 그대로)
+				pathA[i] = Integer.parseInt(stA.nextToken());
+				pathB[i] = Integer.parseInt(stB.nextToken());
+			}
+
+
+			for (int i = 0; i < bcCnt; i++) {
+				st = new StringTokenizer(br.readLine(), " ");
+				bc[i][0] = Integer.parseInt(st.nextToken()); // x좌표
+				bc[i][1] = Integer.parseInt(st.nextToken()); // y좌표
+				bc[i][2] = Integer.parseInt(st.nextToken()); // 사정거리(범위)
+				bc[i][3] = Integer.parseInt(st.nextToken()); // 충전량
+			}
+
+			// print
+			System.out.println("#" + tc + " " + move());
+
+		} // end for tc
+
+	} // end main
+
+	private static int move() {
+		int totalSum = 0;
+
+		// 매 시간마다의 각 위치에서 두 플레이어의 최대 충전량을 계산하여 합산
+		for (int t = 0; t <= M; t++) { // 초기 위치도 충전 시도
+			playerA[0] += dx[pathA[t]];
+			playerA[1] += dy[pathA[t]];
+			playerB[0] += dx[pathB[t]];
+			playerB[1] += dy[pathB[t]];
+
+			// 현 위치에서의 최대 충전량 계산
+			totalSum += getMaxCahrge();
+		}
+
+		return totalSum;
+	}
+
+	private static int check(int a, int x, int y) {
+		return Math.abs(bc[a][0] - x) + Math.abs(bc[a][1] - y) <= bc[a][2] ? bc[a][3] : 0;
+	}
+
+	private static int getMaxCahrge() {
+		int max = 0;
+
+		for (int a = 0; a < bcCnt; a++) { // 플레이어 A가 선택한 BC
+			for (int b = 0; b < bcCnt; b++) { // 플레이어 B가 선택한 BC
+				// 0, 0 / 0, 1 / 0, 2 / 1, 0 / 1, 1 / 1, 2 / 2, 0 / 2, 1 / 2, 2
+				int sum = 0;
+				int amountA = check(a, playerA[0], playerA[1]);
+				int amountB = check(b, playerB[0], playerB[1]);
+
+				// 두 충전소가 다르면
+				if (a != b) sum = amountA + amountB;
+				else sum = Math.max(amountA, amountB);
+
+				if (max < sum) max = sum;
+			}
+		}
+
+		return max;
+	}
+
+}
