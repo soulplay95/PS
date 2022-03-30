9fc650fe47a82eaec8a284a3114e7bbebe85329a
soulplay95 <soulplay95@gmail.com>
2022-03-30 22:43:46 +0900

    feat: Solve PROG_12953

---
+++ BOJ/PROG_12953.java
@@ -0,0 +1,47 @@
+/**
+ * @문제
+ * @날짜 220330
+ * @분류
+ * @조건
+ * #
+ * @풀이
+ * #
+ * @comments
+ * # 정답의 최대치:
+ * # 시간 복잡도:
+ * # 공간 복잡도:
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class PROG_12953 {
+
+    static StringBuilder sb = new StringBuilder();
+
+
+    public static void main(String[] args) throws IOException {
+        input();
+        solve();
+        print();
+    }
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+
+
+    }
+
+    static void solve() {
+
+    }
+
+    static void print() {
+
+    }
+
+}
