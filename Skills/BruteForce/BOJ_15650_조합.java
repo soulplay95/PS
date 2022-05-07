baacfe32e6f3f7d8b401a445d5ebffcf8a1537ea
soulplay95 <soulplay95@gmail.com>
2022-05-07 22:32:29 +0900

    feat: Solve BOJ_15650

---
+++ Skills/BruteForce/BOJ_15650_조합.java
@@ -0,0 +1,73 @@
+package algorithm.brute_force;
+
+/**
+ * @문제 N과 M (2) [S3]
+ * @날짜 220507
+ * @분류 뱩트래킹
+ * @조건
+ * # 1 <= M <= N <= 8
+ * @풀이
+ * # 조합 : 중복없이 고르기
+ * - 전에 뽑은 수의 다음 수부터 고르기
+ * @comments
+ * # 정답의 최대치: Integer => 최대 N 이라는 숫자가 등장 <= 8
+ * # 시간 복잡도: O(NCM) = O(N! / (N - M)!M!) => 8! / 4!4! = 70
+ * # 공간 복잡도: O(M)
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_15650_조합 {
+
+    static StringBuilder sb = new StringBuilder();
+
+    static int N, M;
+    static int[] selected;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        solve();
+        print();
+    }
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        // init
+        selected = new int[M + 1];
+    }
+
+    static void solve() {
+        recursiveFunc(1, 1);
+    }
+
+    static void recursiveFunc(int k, int start) {
+        if (k == M + 1) { // M개를 전부 고름 => 조건에 맞는 탐색을 한 가지 성공!
+            // selected 배열에 저장된 결과 출력
+            for (int i = 1; i <= M; i++) {
+                sb.append(selected[i]).append(' ');
+            }
+            sb.append('\n');
+        } else {
+            // 현재 k번째 숫자는 start부터 뽑기
+            for (int candidate = start; candidate <= N; candidate++) {
+                selected[k] = candidate;
+                recursiveFunc(k + 1, candidate + 1); // 현재 뽑은 수의 다음 수(candidate + 1)를 다음 start로 넘겨줌
+                selected[k] = 0;
+            }
+        }
+    }
+
+    static void print() {
+        System.out.println(sb.toString());
+    }
+
+}
